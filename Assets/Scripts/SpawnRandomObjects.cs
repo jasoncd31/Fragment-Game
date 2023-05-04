85535352999621107defa6389cb02dcc77b1cf6e
Jason Douglas <jasoncd31@gmail.com>
2023-05-04 14:15:53 -0700

    optimize for presentations

--- Assets/Scripts/SpawnRandomObjects.cs
+++ Assets/Scripts/SpawnRandomObjects.cs
@@ -11,17 +11,15 @@ public class SpawnRandomObjects : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (var i = 0; i < numOfObj; i++)
+        for (var i = 0; i < 3; i++)
         {
             //Vector3 rotation = new Vector3(0.0f, Random.Range(0.0f, 180.0f), 0, 0f);
             Instantiate(plants[0], new Vector3(Random.Range(-50.0f,50.0f) + parent.transform.position.x, 0.0f, Random.Range(-50.0f, 50.0f) + parent.transform.position.z), Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f),0.0f));
         }
-
-        for (var i = 0; i < 2; i++)
+        for (var i = 0; i < 1; i++)
         {
             Instantiate(plants[1], new Vector3(Random.Range(-40.0f, 40.0f) + parent.transform.position.x, 0.0f, Random.Range(-40.0f, 40.0f) + parent.transform.position.z), Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
         }
-
         for (var i = 0; i < 1; i++)
         {
             Instantiate(plants[2], new Vector3(Random.Range(-40.0f, 40.0f) + parent.transform.position.x, 0.0f, Random.Range(-40.0f, 40.0f) + parent.transform.position.z), Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f));
